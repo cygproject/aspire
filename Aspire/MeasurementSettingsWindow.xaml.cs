b40e0d21677f6331fab633ffd99b234fba6d26ce
Eman Guiruela <elguiruela@gmail.com>
2018-08-03 16:27:14 +0900

    Added MeasurementSettingsWindow.

---
+++ Aspire/MeasurementSettingsWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Aspire
+{
+    /// <summary>
+    /// Interaction logic for MeasurementSettingsWindow.xaml
+    /// </summary>
+    public partial class MeasurementSettingsWindow : Window
+    {
+        public MeasurementSettingsWindow()
+        {
+            InitializeComponent();
+
+            cmbInterval.ItemsSource = new string[] {
+                "100",
+                "200",
+                "500",
+                "1000"
+            };
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Accept the dialog and return the dialog result
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Accept the dialog and return the dialog result
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}
