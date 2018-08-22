9b75a6e541d11b912ecb90a943ee9cd9ce98e924
Eman Guiruela <elguiruela@gmail.com>
2018-08-22 09:43:35 +0900

    Updated values of cmbInterval to faster intervals.

--- Aspire/ToolWindows/MeasurementSettingsWindow.xaml.cs
+++ Aspire/ToolWindows/MeasurementSettingsWindow.xaml.cs
@@ -24,11 +24,12 @@ namespace Aspire
             InitializeComponent();
 
             cmbInterval.ItemsSource = new string[] {
+                "2",
+                "5",
+                "10",
+                "20",
                 "50",
                 "100",
-                "200",
-                "500",
-                "1000"
             };
 
             cmbEnableLog.ItemsSource = new string[] {
